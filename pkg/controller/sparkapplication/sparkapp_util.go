kubeflow/spark-operator

Support for SSL enabled Driver UI

pkg/controller/sparkapplication/sparkapp_util.go
@@ -114,3 +114,13 @@ func driverStateToApplicationState(podStatus apiv1.PodStatus) v1beta1.Applicatio
 		return v1beta1.UnknownState
 	}
 }
+
+func getSvcAnnotations(app *v1beta1.SparkApplication) map[string]string {
+	annotations := map[string]string{}
+	if app.Spec.SparkConf["spark.ssl.ui.enabled"] == "true" {
+		annotations["nginx.ingress.kubernetes.io/backend-protocol"] = "HTTPS"
+	} else {
+		annotations["nginx.ingress.kubernetes.io/backend-protocol"] = "HTTP"
+	}
+	return annotations
+}

nokia/spark-on-k8s-operator

pkg/controller/sparkapplication/sparkapp_util.go
@@ -143,3 +143,13 @@ func getVolumeFSType(v v1.Volume) (policy.FSType, error) {
 
 	return "", fmt.Errorf("unknown volume type for volume: %#v", v)
 }
+
+func getSvcAnnotations(app *v1beta2.SparkApplication) map[string]string {
+	annotations := map[string]string{}
+	if app.Spec.SparkConf["spark.ssl.ui.enabled"] == "true" {
+		annotations["nginx.ingress.kubernetes.io/backend-protocol"] = "HTTPS"
+	} else {
+		annotations["nginx.ingress.kubernetes.io/backend-protocol"] = "HTTP"
+	}
+	return annotations
+}